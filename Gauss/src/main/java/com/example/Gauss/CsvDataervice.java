573c8695620b014e0a8b14b2efe7c82aebadbc9c
Arturo <artuloca@myuax.co>
2024-10-12 13:33:02 +0200

    commit importacion de datos

--- Gauss/src/main/java/com/example/Gauss/CsvDataervice.java
+++ Gauss/src/main/java/com/example/Gauss/CsvDataervice.java
@@ -13,7 +13,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
 @Service
-public class CsvDataService {
+public class CsvDataervice {
 
     @Autowired
     private CsvDataRepository csvDataRepository;
