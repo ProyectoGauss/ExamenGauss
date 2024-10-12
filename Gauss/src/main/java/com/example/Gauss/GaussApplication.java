c5684b208b9fbba6ec168078edbed1f11024c912
Arturo <artuloca@myuax.co>
2024-10-12 12:23:01 +0200

    commit inicial

---
+++ Gauss/src/main/java/com/example/Gauss/GaussApplication.java
@@ -0,0 +1,13 @@
+package com.example.Gauss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaussApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaussApplication.class, args);
+	}
+
+}
